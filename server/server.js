2960c0358c9d78dfb7eec420b383147b5184db54
Kevin Schestakow <ks209@hdm-stuttgart.de>
2024-06-09 16:36:57 +0200

    refactor colsahpe npc pos in json

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import { handlePlayerConnect, handlePlayerDisconnect } from './playerConnectHand
 import { registerChatCommands, teleportPlayer } from './chatCommandsHandler.js';
 import { handlePlayerDeath } from './deathHandler.js';
 import { loadConfig } from '../helper/configLoader.js';
+import { createColshapeAndMarker } from './ffaLogic.js';
 
 const config = loadConfig('configs/positions.json');
 
@@ -13,8 +14,8 @@ alt.on('playerDamage', handlePlayerDamage);
 alt.on('playerDeath', handlePlayerDeath);
 
 registerChatCommands();
+createColshapeAndMarker(config.ffaColshape, config.ffaMarker);
 
-// teleport menu
 alt.onClient('teleportPlayerToLocation', (player, position) => {
     teleportPlayer(player, position, player.dimension);
 });
