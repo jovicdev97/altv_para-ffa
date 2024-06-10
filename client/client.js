4623a628176181c9043d8654a080682519930a14
Kevin Schestakow <ks209@hdm-stuttgart.de>
2024-06-11 01:21:07 +0200

    add: webview

--- client/client.js
+++ client/client.js
@@ -1,7 +1,8 @@
-// client.js
 import * as alt from 'alt-client';
 import { handleSyncedMetaChange } from './handleSyncedMetaChange.js';
 import { handleEntityEnterColshape, handleEntityLeaveColshape, handleCreateMarker } from './zoneHandler.js';
+
+let webview = null;
 
 // Listen for synced meta changes : log FFA status
 alt.on('syncedMetaChange', handleSyncedMetaChange);
@@ -12,3 +13,26 @@ alt.onServer('entityLeaveColshape', handleEntityLeaveColshape);
 
 // Listen for server event : create Marker
 alt.onServer('createMarker', handleCreateMarker);
+
+alt.onServer('showWebView', (url) => {
+    alt.log('Showing webview with URL:', url);
+    if (!url) {
+        alt.log('Error: URL is undefined');
+        return;
+    }
+
+    if (webview) return;
+
+    webview = new alt.WebView(url, true);
+    webview.focus();
+    alt.showCursor(true);
+    alt.toggleGameControls(false);
+
+    webview.on('closeWebView', () => {
+        alt.log('Closing webview');
+        alt.showCursor(false);
+        alt.toggleGameControls(true);
+        webview.destroy();
+        webview = null;
+    });
+});
--- server/commands/chatCommands.js
+++ server/commands/chatCommands.js
@@ -1,14 +1,22 @@
+import * as alt from 'alt-server';
 import * as chat from 'alt:chat';
-import { handleFFACommand, handleExitFFACommand} from './ffaCommands.js';
+import { handleFFACommand, handleExitFFACommand } from './ffaCommands.js';
 import '../colShapeEvents.js';
 
 export function registerChatCommands() {
     chat.registerCmd('ffa', handleFFACommand);
     chat.registerCmd('exitffa', handleExitFFACommand);
+    chat.registerCmd('webview', handleWebViewCommand);
 }
 
 chat.registerCmd('coords', (player) => {
     let coords = player.pos;
-    alt.log(coords + (" from alt console"));
+    alt.log(coords + " from alt console");
     console.log(coords + " from console");
 });
+
+const webviewUrl = 'http://resource/client/html/index.html';
+function handleWebViewCommand(player) {
+    alt.log('Received /webview command from player:', player.name);
+    alt.emitClient(player, 'showWebView', webviewUrl);
+}
