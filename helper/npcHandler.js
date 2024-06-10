46eeb59a5433d1bdb67392051ead3e055870c6f1
Kevin Schestakow <ks209@hdm-stuttgart.de>
2024-06-10 16:01:13 +0200

    add: some more npc configs

--- helper/npcHandler.js
+++ helper/npcHandler.js
@@ -11,10 +11,15 @@ if (config && config.npc_positions) {
 
 export function createNPC(position) {
     try {
-        const npc = new alt.Ped('S_M_Y_PestCont_01', position, 0);
-        npc.dimension = position.dimension || 0;
+        const model = position.model || 'S_M_Y_PestCont_01';
+        const rotation = position.rotation || { x: 0, y: 0, z: 0 };
+        const streamingDistance = position.streamingDistance || undefined;
+
+        const npc = new alt.Ped(model, position, rotation, streamingDistance);
+        npc.dimension = position.dimension || 0;  
         npc.setMeta('visible', true);
-        alt.log(`NPC created at (${position.x}, ${position.y}, ${position.z}) in dimension ${npc.dimension}`);
+
+        alt.log(`NPC created at (${position.x}, ${position.y}, ${position.z}) in dimension ${npc.dimension} with model ${model}`);
     } catch (error) {
         console.error('Error creating NPC:', error);
     }
